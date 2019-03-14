f1eebf339ca5e63b0c1e6692ecd6f4f99a7f9e1d
PhistucK <phistuck@gmail.com>
2019-03-14 19:29:54 +0200

    Changed the base URL to be relative

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL =  'http://914e5464.ngrok.io';
+axios.defaults.baseURL =  '/';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
